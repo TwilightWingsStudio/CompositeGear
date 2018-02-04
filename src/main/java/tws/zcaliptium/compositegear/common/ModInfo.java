4ea52c52129005b0832619f795a9636854c960c4
zcaliptium <zcaliptium@gmail.com>
2018-02-05 00:35:37 +0300

    Change version in the ModInfo class.

--- src/main/java/tws/zcaliptium/compositegear/common/ModInfo.java
+++ src/main/java/tws/zcaliptium/compositegear/common/ModInfo.java
@@ -10,5 +10,5 @@ package tws.zcaliptium.compositegear.common;
 public class ModInfo {
     public static final String MODID = "compositegear";
     public static final String MODNAME = "Composite Gear";
-    public static final String VERSION = "1.7.10";
+    public static final String VERSION = "1.12.2";
 }
